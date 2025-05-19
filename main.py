f437ec855285653de482d2f35506a080b573d89f
Ricardo Junio <ricardomarinho.vaga@gmail.com>
2025-05-19 21:09:26 +0000

    primeiro commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+
+#cria janela
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Juninho's Fighter")
+
+#carrega imagem de fundo
+background = pygame.image.load("assets/Background/background1.png").convert_alpha()
+
+#função para desenhar o fundo
+def draw_background():
+    scaled_background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT)) 
+    screen.blit(scaled_background, (0, 0))
+
+#loop para manter a janela aberta
+run = True
+while run:
+
+    #desenha o fundo
+    draw_background()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False 
+
+    #atualiza a tela
+    pygame.display.update()
+
+#sai do pygame
+pygame.quit()
