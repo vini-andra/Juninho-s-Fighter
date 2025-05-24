eda91c99070bee163053897ce0ffc95f9c9fd4f5
Vinicius Armando Menezes de Andrade <v.armandomenezes@gmail.com>
2025-05-23 21:03:12 -0300

    47:34 momento que parei

--- main.py
+++ main.py
@@ -14,6 +14,12 @@ pygame.display.set_caption("Juninho's Fighter")
 clock = pygame.time.Clock()
 fps = 60
 
+#define as cores
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+
+
 #carrega imagem de fundo
 background = pygame.image.load("assets/Background/background1.jpeg").convert_alpha()
 
@@ -21,6 +27,13 @@ background = pygame.image.load("assets/Background/background1.jpeg").convert_alp
 def draw_background():
     scaled_background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT)) 
     screen.blit(scaled_background, (0, 0))
+
+#funcao das barras de vida
+def draw_health_bar(health, x, y):
+    ratio = health / 100
+    pygame.draw.rect(screen, WHITE, (x - 2,y - 2, 404, 34))
+    pygame.draw.rect(screen, RED, (x, y, 400, 30))
+    pygame.draw.rect(screen, YELLOW, (x, y, 400 * ratio, 30))
 
 #cria instâncias de jogador
 player_1 = Fighter(200, 300) #posição x e y, para aparecer na tela
@@ -35,9 +48,15 @@ while run:
 
     #desenha o fundo
     draw_background()
+    
+    #mostra os status do personagem
+    draw_health_bar(player_2.health, 20, 20)
+    draw_health_bar(player_1.health, 580, 20)
+    
 
     #move os personagens
-    player_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, player_2)
+    player_1.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, player_1)
+    #player_2.move(SCREEN_WIDTH, SCREEN_HEIGHT, screen, player_2)
 
     #desenha os lutadores
     player_1.draw(screen)
